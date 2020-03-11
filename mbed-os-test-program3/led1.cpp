501ef1d7d7bec880fdee48661ba3984bdc4695e2
chenhanlee <ya060505@gmail.com>
2020-03-11 18:24:42 +0800

    add led1.cpp led2.cpp main.cpp

---
+++ mbed-os-test-program3/led1.cpp
@@ -0,0 +1,16 @@
+#include "mbed.h"
+
+
+void Led1(DigitalOut& ledpin)
+
+{
+
+   for(int i=0; i<6; ++i) { //blink for 10 times
+
+       ledpin = !ledpin; // toggle led
+
+       wait(0.2f);
+
+   }
+
+}
---
+++ mbed-os-test-program3/led2.cpp
@@ -0,0 +1,16 @@
+#include "mbed.h"
+
+
+void Led2(DigitalOut& ledpin)
+
+{
+
+   for(int i=0; i<4; ++i) { //blink for 10 times
+
+       ledpin = !ledpin; // toggle led
+
+       wait(0.2f);
+
+   }
+
+}
